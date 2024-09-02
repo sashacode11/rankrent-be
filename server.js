4e7fa234ee4dc075f1c29316cc4ddb1a7ba9d307
Sasha <chan.sasha16@gmail.com>
2024-09-02 08:47:12 +0700

    added locality list

--- server.js
+++ server.js
@@ -6,18 +6,22 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+let data = {};
 app.get('/', (req, res) => {
-  let data = {};
-
   fs.createReadStream('locality.csv')
     .pipe(csv())
     .on('data', row => {
       if (!data[row.region]) {
         data[row.region] = [];
       }
-      if (!data[row.locality]) {
-        data[row.locality] = [];
-      }
+      data[row.region].push({ locality: row.locality });
+
+      //   if (!data[row.region]) {
+      //     data[row.region] = [];
+      //   }
+      //   if (!data[row.locality]) {
+      //     data[row.locality] = [];
+      //   }
     })
     .on('end', () => {
       res.render('index', { data: data });
