65bdb93ceacd0b1b8c6b38c1dcb4c48e9591cb04
Sasha <chan.sasha16@gmail.com>
2024-09-04 12:09:57 +0700

    added invoice confim page

--- public/js/update-amount.js
+++ public/js/update-amount.js
@@ -1,32 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//   const termSelect = document.getElementById('termSelect');
-//   const amountDisplay = document.getElementById('amountDisplay');
-
-//   function updateAmount() {
-//     const selectedTerm = termSelect.value;
-//     const amount = window.termsToAmounts[selectedTerm];
-//     amountDisplay.textContent = 'Amount: $' + (amount || '0.00');
-//   }
-
-//   // Attach event listener to the term select dropdown
-//   termSelect.addEventListener('change', updateAmount);
-
-//   // Initialize the display on load in case there's a default selection
-//   updateAmount();
-// });
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   updatePrice(); // Initial price update
-// });
-
-// function updatePrice() {
-//   const productSelect = document.getElementById('productSelect');
-//   const priceDisplay = document.getElementById('priceDisplay');
-//   const selectedPrice = productSelect.value;
-
-//   priceDisplay.textContent = `US$${selectedPrice}/mo`;
-// }
-
 document
   .getElementById('emailForm')
   .addEventListener('submit', function (event) {
@@ -45,31 +16,27 @@ document
     const locality = document.getElementById('locality').value;
 
     // Update modal contents
-    document.getElementById(
-      'modalCustomer'
-    ).textContent = `Customer Email: ${customerEmail}`;
+    document.getElementById('modalCustomer').textContent = `${customerEmail}`;
     document.getElementById(
       'modalTerm'
-    ).textContent = `Term Selected: ${termSelect} - US$${termAmount}`;
-    document.getElementById('modalRegion').textContent = `Region: ${region}`;
-    document.getElementById('modalNiche').textContent = `Niche: ${niche}`;
-    document.getElementById(
-      'modalLocality'
-    ).textContent = `Locality: ${locality}`;
-    document.getElementById(
-      'modalSubtotal'
-    ).textContent = `Subtotal: US$${termAmount}`; // Display the amount
+    ).textContent = `${termSelect} - US$${termAmount}`;
+    document.getElementById('modalRegion').textContent = `${region}`;
+    document.getElementById('modalNiche').textContent = `${niche}`;
+    document.getElementById('modalLocality').textContent = `${locality}`;
+    document.getElementById('modalSubtotal').textContent = `US$${termAmount}`; // Display the amount
 
     // Display the modal
     document.getElementById('modal').style.display = 'block';
+    // document.getElementById('modal').classList.add('open');
   });
 
 function closeModal() {
   document.getElementById('modal').style.display = 'none';
+  // document.getElementById('modal').classList.remove('open');
 }
 
-function confirmSubmission() {
-  // Handle the confirmation of the form submission here
-  closeModal();
-  alert('Email would be sent here');
-}
+// function confirmSubmission() {
+//   // Handle the confirmation of the form submission here
+//   closeModal();
+//   alert('Email would be sent here');
+// }
